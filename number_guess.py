e8a2078613a37deb71daeaa00a648a7d3f8c715b
John Fessler <jffessler@gmail.com>
2023-07-31 23:09:58 -0700

    issues with keeping random number within bound

--- number_guess.py
+++ number_guess.py
@@ -3,24 +3,46 @@ class randomNumber:
     def __init__(self, lower_bound, upper_bound):
         self.lower_bound = lower_bound
         self.upper_bound = upper_bound
-        set={range(self.lower_bound,self.upper_bound+1)}
-        if self.lower_bound or self.upper_bound < 0:
+        bounds_set = set()
+        random_number_str = ""
+        random_number = -1
+
+        def oneDigit():
+            #import time
+            import time
+            #use time method to generate a random number from 0-9
+            current_time = time.time()
+            time_string = repr(current_time)
+            final_str_num = time_string[-2]
+            # final_num = int(final_str_num)
+            # print(final_num)
+            return final_str_num
+        
+        #building the random number
+        bounds = self.upper_bound + 1 - self.lower_bound
+        str_bounds = repr(bounds)
+
+        if self.lower_bound < 0 or self.upper_bound < 0:
             return print("Range must be greater than or equal to 0")
-        elif self.lower_bound >= 0 and self.upper_bound < 10:
-            None
         else:
-            None
-            
+            #making a set of all potential numbers
+            for x in range(self.lower_bound, self.upper_bound+1):
+                bounds_set.add(x)
+                print(bounds_set)
 
+        while random_number not in bounds_set:
+                #selecting random digits
+                print(random_number)
+                for x in range(len(str_bounds)):
+                    print(f'loop {x}')
+                    one_digit = oneDigit()
+                    print(f'one_digit {one_digit}')
+                    random_number_str += one_digit
+                random_number = int(random_number_str)
+                # print(random_number)
 
-    def oneDigit():
-        #import time
-        import time
-        #use time method to generate a random number from 0-9
-        current_time = time.time()
-        time_string = repr(current_time)
-        final_str_num = time_string[-1]
-        final_num = int(final_str_num)
-        print(final_num)
+        return print(random_number)
+        
+        
 
 number = randomNumber(1,10)
